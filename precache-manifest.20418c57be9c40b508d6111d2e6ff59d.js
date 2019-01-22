187f7dcef38fd31bf34cf526ebd1921342b54450
mostrowski8 <mostrowski8@gmail.com>
2019-01-22 22:58:47 +0100

    Updates

---
+++ precache-manifest.20418c57be9c40b508d6111d2e6ff59d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/bodyfat/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "9cb2868c0184faf22e24",
+    "url": "/bodyfat/static/js/runtime~main.9cb2868c.js"
+  },
+  {
+    "revision": "8547b450286f879a435e",
+    "url": "/bodyfat/static/js/main.8547b450.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/bodyfat/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "8547b450286f879a435e",
+    "url": "/bodyfat/static/css/main.2691edba.chunk.css"
+  },
+  {
+    "revision": "d7c407d2548f6e8dc173916353864dd1",
+    "url": "/bodyfat/index.html"
+  }
+];
