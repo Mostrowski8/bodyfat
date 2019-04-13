a6456988bcdda08b1d80562ad4c7b088b90820a8
mostrowski8 <mostrowski8@gmail.com>
2019-04-13 20:13:37 +0200

    Updates

--- precache-manifest.68304714b9e63dd4b594c895fe627130.js
+++ precache-manifest.68304714b9e63dd4b594c895fe627130.js
@@ -4,23 +4,23 @@ self.__precacheManifest = [
     "url": "/bodyfat/static/js/runtime~main.9cb2868c.js"
   },
   {
-    "revision": "a424ce4d1efba334355c",
-    "url": "/bodyfat/static/js/main.a424ce4d.chunk.js"
+    "revision": "75c14516cb8584a26b02",
+    "url": "/bodyfat/static/js/main.75c14516.chunk.js"
   },
   {
     "revision": "b3ce9011547eca3e97bd",
     "url": "/bodyfat/static/js/1.b3ce9011.chunk.js"
   },
   {
-    "revision": "a424ce4d1efba334355c",
-    "url": "/bodyfat/static/css/main.add5e940.chunk.css"
+    "revision": "75c14516cb8584a26b02",
+    "url": "/bodyfat/static/css/main.a37dccac.chunk.css"
   },
   {
     "revision": "b3ce9011547eca3e97bd",
     "url": "/bodyfat/static/css/1.4903832c.chunk.css"
   },
   {
-    "revision": "2aa461385a2fcd67b329af81a852338e",
+    "revision": "e906b7cad7b482627d441aed2dde0ebd",
     "url": "/bodyfat/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/bodyfat/precache-manifest.73bcec28ab39b51d27c2cd412b34f465.js"
+  "/bodyfat/precache-manifest.68304714b9e63dd4b594c895fe627130.js"
 );
 
 workbox.clientsClaim();
